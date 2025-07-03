209bafb0a18a1bba53106cf336b84bcc5e3d7d8d
Guest Student <contact.dje@gmail.com>
2025-07-03 11:15:40 +0000

    first try

--- hasher.py
+++ hasher.py
@@ -3,6 +3,11 @@ import hashlib
 def hash(unhashed_string):
     return hashlib.md5(unhashed_string.encode('utf-8')).hexdigest()
 
-while True:
-    entered_string = input("Please input the string: ")
-    print(hash(entered_string))
+# while True:
+#     entered_string = input("Please input the string: ")
+#     print(hash(entered_string))
+counter=1
+while counter <=9999:
+    if hash("{:04d}".format(counter))=="43dd49b4fdb9bede653e94468ff8df1e":
+        print(counter)
+        counter+=1
