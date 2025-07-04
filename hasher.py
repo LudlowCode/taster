ba5b0615076531adf94bb811d71e569190b0073f
Dave Edwards <edwardsd@hlcollege.ac.uk>
2025-07-04 09:47:52 +0100

    add padder

--- hasher.py
+++ hasher.py
@@ -3,6 +3,9 @@ import hashlib
 def hash(unhashed_string):
     return hashlib.md5(unhashed_string.encode('utf-8')).hexdigest()
 
+def pad_to_4_digits(int_to_pad):
+    return str(int_to_pad).zfill(4)
+
 while True:
-    entered_string = input("Please input the string: ")
+    entered_string = input("Please guess the secret to receive a hash digest: ")
     print(hash(entered_string))
