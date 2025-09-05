33859a9f04061d28ab81547133ea71b7e767d8f7
Dave Edwards <edwardsd@hlcollege.ac.uk>
2025-09-05 09:39:00 +0100

    sfgxfggxf

--- hasher.py
+++ hasher.py
@@ -5,7 +5,6 @@ def hash(unhashed_string):
 
 def pad_to_4_digits(int_to_pad):
     return str(int_to_pad).zfill(4)
-    print("HI") 
 
 while True:
     entered_string = input("Please guess the secret to receive a hash digest: ")
